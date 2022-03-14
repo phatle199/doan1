934e5eb02286ca5903917b67daba3b8096150e0e
Phat Le <vietphatt1909@gmail.com>
2022-03-14 19:00:48 +0700

    fix bug cho nút next và prev trong phân trang

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9529,32 +9529,33 @@ if (previousButton) {
       pageNumberItems.forEach(function (item) {
         item.setAttribute('href', item.getAttribute('href').replace("page=".concat(item.innerHTML), "page=".concat(Number(item.innerHTML) - 1)));
         item.innerText = +item.innerHTML - 1;
+      }); // 3. Xóa class disabled của nút next
+
+      nextButton.classList.remove('disabled'); // 4. Nếu nút trang đầu tiên giảm còn 1 => thêm class disabled vào nút prev
+
+      if (Number(pageNumberItems[0].innerText) === 1) {
+        previousButton.classList.add('disabled');
+      } // 5. Thêm class active nếu nút đó có giá trị bằng với trang hiện tại
+
+
+      var pageNumberParam = Number((0, _getParamsObject.default)().page);
+      var currentPageNumber = pageNumberParam === 0 || pageNumberParam < 1 ? 1 : pageNumberParam;
+      pageNumberItems.forEach(function (item) {
+        if (Number(item.innerText) === currentPageNumber) {
+          item.closest('li').classList.add('active');
+        } else {
+          item.closest('li').classList.remove('active');
+        }
       });
-    } // 3. Xóa class disabled của nút next
-
-
-    nextButton.classList.remove('disabled'); // 4. Nếu nút trang đầu tiên giảm còn 1 => thêm class disabled vào nút prev
-
-    if (Number(pageNumberItems[0].innerText) === 1) {
-      previousButton.classList.add('disabled');
-    } // 5. Thêm class active nếu nút đó có giá trị bằng với trang hiện tại
-
-
-    var pageNumberParam = Number((0, _getParamsObject.default)().page);
-    var currentPageNumber = pageNumberParam === 0 || pageNumberParam < 1 ? 1 : pageNumberParam;
-    pageNumberItems.forEach(function (item) {
-      if (Number(item.innerText) === currentPageNumber) {
-        item.closest('li').classList.add('active');
-      } else {
-        item.closest('li').classList.remove('active');
-      }
-    });
+    }
   });
 }
 
 if (nextButton) {
   // 1. Nếu nút chọn trang cuối cùng bằng với trang cuối => ko thể có thêm các trang sau nó
   var lastPageNumber = +document.querySelectorAll('.page-item')[3].innerText;
+  console.log('lastPageNumber', lastPageNumber);
+  console.log('pageSize', pageSize);
 
   if (lastPageNumber === pageSize) {
     nextButton.classList.add('disabled');
@@ -9568,33 +9569,32 @@ if (nextButton) {
       pageNumberItems.forEach(function (item) {
         item.setAttribute('href', item.getAttribute('href').replace("page=".concat(item.innerHTML), "page=".concat(Number(item.innerHTML) + 1)));
         item.innerText = +item.innerHTML + 1;
-      });
-    } // 3. Xóa class disabled của previousButton khi nút trang đầu tiên ko còn là 1 nữa
+      }); // 3. Xóa class disabled của previousButton khi nút trang đầu tiên ko còn là 1 nữa
+
+      previousButton.classList.remove('disabled'); // 4. Xóa class active của nút trang đang được chọn, thêm class active vào nút trước nó
+
+      var selectedPageNumberItem = document.querySelector('.page-item.active');
+      var pageNumberParam = Number((0, _getParamsObject.default)().page);
+      var currentPageNumber = pageNumberParam === 0 || pageNumberParam < 1 ? 1 : pageNumberParam;
+
+      if (selectedPageNumberItem) {
+        // Nếu nút trước nút trang được chọn bằng với trang hiện tại => thêm class active
+        var previousSelectedPageNumberItem = selectedPageNumberItem.previousSibling;
+
+        if (previousSelectedPageNumberItem && currentPageNumber === Number(previousSelectedPageNumberItem.innerText)) {
+          previousSelectedPageNumberItem.classList.add('active');
+        } // Xóa class active
 
 
-    previousButton.classList.remove('disabled'); // 4. Xóa class active của nút trang đang được chọn, thêm class active vào nút trước nó
-
-    var selectedPageNumberItem = document.querySelector('.page-item.active');
-    var pageNumberParam = Number((0, _getParamsObject.default)().page);
-    var currentPageNumber = pageNumberParam === 0 || pageNumberParam < 1 ? 1 : pageNumberParam;
-
-    if (selectedPageNumberItem) {
-      // Nếu nút trước nút trang được chọn bằng với trang hiện tại => thêm class active
-      var previousSelectedPageNumberItem = selectedPageNumberItem.previousSibling;
-
-      if (previousSelectedPageNumberItem && currentPageNumber === Number(previousSelectedPageNumberItem.innerText)) {
-        previousSelectedPageNumberItem.classList.add('active');
-      } // Xóa class active
+        selectedPageNumberItem.classList.remove('active');
+      } // 5. Thêm class disabled khi nút trang cuối bằng với số trang
 
 
-      selectedPageNumberItem.classList.remove('active');
-    } // 5. Thêm class disabled khi nút trang cuối bằng với số trang
+      lastPageNumber = +document.querySelectorAll('.page-item')[3].innerText;
 
-
-    lastPageNumber = +document.querySelectorAll('.page-item')[3].innerText;
-
-    if (lastPageNumber === pageSize) {
-      nextButton.classList.add('disabled');
+      if (lastPageNumber === pageSize) {
+        nextButton.classList.add('disabled');
+      }
     }
   });
 } // AUTH
