214c4226009ed9ba8793b4e557eddd4173b5a806
Phat Le <vietphatt1909@gmail.com>
2022-04-18 19:12:39 +0700

    Viết controller xử lý việc lấy checkout session

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const tourRouter = require('./tourRouter');
 const userRouter = require('./userRouter');
 const reviewRouter = require('./reviewRouter');
+const bookingRouter = require('./bookingRouter');
 const viewRouter = require('./viewRouter');
 
 const router = (app) => {
   app.use('/api/v1/tours', tourRouter);
   app.use('/api/v1/users', userRouter);
   app.use('/api/v1/reviews', reviewRouter);
+  app.use('/api/v1/bookings', bookingRouter);
   app.use('/', viewRouter);
 };
 
